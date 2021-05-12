8d4e1d377ab00f5058e98d963ecc4a61b6f3e4e6
bauhausk <andrea@xkoll.com>
2021-05-12 20:29:21 +0200

    Dinamic Array

---
+++ src/it/esedra/corso/esercizi/DinArray.java
@@ -0,0 +1,18 @@
+public class DinArray {
+  public static void main(String[] args) {
+
+
+
+
+  }
+
+
+
+  public String[] add(String[] inputArray, String newElem) {
+    if(inputArray == null) {
+      inputArray = new String[1];
+      inputArray[0] = newElem;
+    }
+    return inputArray;
+  }
+}
