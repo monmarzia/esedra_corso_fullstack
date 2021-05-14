f893f6a556df7e421e2b81380f6851e6247bf583
bauhausk <andrea@xkoll.com>
2021-05-14 19:04:58 +0200

    test case

---
+++ src/TestCase.java
@@ -0,0 +1,16 @@
+import it.esedra.corso.lib.DinTest;
+
+public class TestCase  {
+
+
+  /**
+  * Uso questo metodo per inizializzare la mia applicazione esegute tutti i test
+  * @param args: argomenti
+  */
+  public static void main (/* argomenti */String[] args) {
+    String[] argument = new String[0];
+    //richiamo DinTest
+    DinTest.main(argument);
+  }
+
+}
