db4a666e25dcc10162de24335992b717323ea199
ThomasMarchese <Thomasmarkese@hotmail.it>
2021-06-23 19:39:07 +0200

    primo esercizio

--- src/it/esedra/corso/esercitazioneB/thomasmarchese/Distributore.java
+++ src/it/esedra/corso/esercitazioneB/thomasmarchese/Distributore.java
@@ -29,21 +29,28 @@ public class Distributore {
 	
 	
 	
-	
 	public static void main(String[] args) {
 		
 		Automobile[] auto = new Automobile[5];
-		auto[0].serbatoio.setCapacita(5);
-		auto[1].serbatoio.setCapacita(21);
-		auto[2].serbatoio.setCapacita(7);
-		auto[3].serbatoio.setCapacita(4);
-		auto[4].serbatoio.setCapacita(11);
+				
+		Automobile auto1 = new Automobile("dskl",5);
+		Automobile auto2 = new Automobile("dwahb",21);
+		Automobile auto3 = new Automobile("lokw",7);
+		Automobile auto4 = new Automobile("fmwek",4);
+		Automobile auto5 = new Automobile("reka",11);
 		
-		System.out.println("serbatoio 1a : "+auto[0].serbatoio.getCarica());
-		System.out.println("serbatoio 2a : "+auto[1].serbatoio.getCarica());
-		System.out.println("serbatoio 3a : "+auto[2].serbatoio.getCarica());
-		System.out.println("serbatoio 4a : "+auto[3].serbatoio.getCarica());
-		System.out.println("serbatoio 5a : "+auto[4].serbatoio.getCarica());
+		auto[0] = auto1;
+		auto[1] = auto2;
+		auto[2] = auto3;
+		auto[3] = auto4;
+		auto[4] = auto5;
+		
+	
+		System.out.println("serbatoio 1a : " + auto[0].serbatoio.getCarica());
+		System.out.println("serbatoio 2a : " + auto[1].serbatoio.getCarica());
+		System.out.println("serbatoio 3a : " + auto[2].serbatoio.getCarica());
+		System.out.println("serbatoio 4a : " + auto[3].serbatoio.getCarica());
+		System.out.println("serbatoio 5a : " + auto[4].serbatoio.getCarica());
 	
 		
 	}
