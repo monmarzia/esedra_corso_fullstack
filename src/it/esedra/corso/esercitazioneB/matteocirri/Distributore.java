1129d39396552746af41ec4528ea9ec09bb6b9bd
MatteoCirri <matteo.cirri96@hotmail.it>
2021-06-23 19:43:19 +0200

    EsercitazioneAuto

--- src/it/esedra/corso/esercitazioneB/matteocirri/Distributore.java
+++ src/it/esedra/corso/esercitazioneB/matteocirri/Distributore.java
@@ -3,16 +3,10 @@ package it.esedra.corso.esercitazioneB.matteocirri;
 public class Distributore {
 	
 	public void rifornimento (Automobile automobile) throws CapacitaSuperata {
-		try {
 			if (automobile.getSerbatoio().getCapacita() < automobile.getSerbatoio().getCarica()) {
 				throw new CapacitaSuperata();
 			}
 			System.out.println(automobile.getSerbatoio().getCarica());
-		}
-		catch (CapacitaSuperata exc) {
-			System.out.println(exc.toString());
-		}
-		
 	}
 
 	public static void main(String[] args) {
@@ -21,8 +15,12 @@ public class Distributore {
 		Automobile auto3 = new Automobile("ccc", 7);
 		Automobile auto4 = new Automobile("ddd", 4);
 		Automobile auto5 = new Automobile("eee", 11);
-		rifornimento(auto1);
-		
+		Distributore distributore = new Distributore();
+		try {
+			distributore.rifornimento(auto1);
+		}
+		catch (CapacitaSuperata exc) {
+			System.out.println(exc.toString());
+		}
 	}
-
 }
