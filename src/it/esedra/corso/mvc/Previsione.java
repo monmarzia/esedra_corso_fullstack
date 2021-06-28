42695c8b57a0647a07e60fdbcd22f933ac617096
Federico <corvino.federico30@gmail.com>
2021-06-28 19:18:02 +0200

    oggetto previsione

--- src/it/esedra/corso/mvc/Previsione.java
+++ src/it/esedra/corso/mvc/Previsione.java
@@ -1,5 +1,21 @@
 package it.esedra.corso.mvc;
 
 public class Previsione {
+	private String tempo;
+	private Float temperatura;
+	
+	public String getTempo() {
+		return tempo;
+	}
+	public void setTempo(String tempo) {
+		this.tempo = tempo;
+	}
+	public Float getTemperatura() {
+		return temperatura;
+	}
+	public void setTemperatura(Float temperatura) {
+		this.temperatura = temperatura;
+	}
+	
 
 }
