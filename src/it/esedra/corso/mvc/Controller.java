33a53fe4f7f09284488277185888365557a869fa
frared91 <francesco.red391@hotmail.com>
2021-06-28 18:47:30 +0200

    controller

--- src/it/esedra/corso/mvc/Controller.java
+++ src/it/esedra/corso/mvc/Controller.java
@@ -8,18 +8,23 @@ public class Controller {
 	public static void main(String[] args) {
 		
 		// http://miosito.it/previsioni-tempo/Firenze
-		URI uri;
+		URI[] uriarray = new URI[3];
 		try {
-			uri = new URI("http://miosito.it/previsioni-tempo/Prato?param=6");
+			uriarray[0] = new URI("http://miosito.it/previsioni-tempo/Prato?param=6");
+			uriarray[1] = new URI("http://miosito.it/previsioni-tempo/Siena?param=6");
+			uriarray[2] = new URI("http://miosito.it/previsioni-tempo/Grosseto?param=6");
 			
-			String[] segments = uri.getPath().split("/");
+			
+		for (URI variabiletemporanea : uriarray) {	
+	
+			String[] segments = variabiletemporanea.getPath().split("/");
 			String pagina = segments[1];
 			String citta = segments[2];
 			Controller controller = new Controller();
 			if (pagina.equals("previsioni-tempo")) {
 				controller.getPrevisioniTempo(citta);
 			}
-
+		}
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
