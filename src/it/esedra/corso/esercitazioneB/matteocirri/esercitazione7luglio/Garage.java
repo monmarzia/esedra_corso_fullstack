be26831eaed23f833f7d7021e9fb059a370fe9c5
MatteoCirri <matteo.cirri96@hotmail.it>
2021-07-07 16:54:42 +0200

    Esercitazione7luglio

--- src/it/esedra/corso/esercitazioneB/matteocirri/esercitazione7luglio/Garage.java
+++ src/it/esedra/corso/esercitazioneB/matteocirri/esercitazione7luglio/Garage.java
@@ -2,11 +2,11 @@ package it.esedra.corso.esercitazioneB.matteocirri.esercitazione7luglio;
 
 import java.util.ArrayList;
 
-public class Garage {
-	private ArrayList<Veicoli> veicoli;	
+public class Garage<T> {
+	private ArrayList<T> veicoli;	
 
 	public static void main(String[] args) {
-		Garage garage = new Garage(); 
+		Garage<Veicoli> garage = new Garage<Veicoli>(); 
 		garage.veicoli.add(new Moto("Ducati"));
 		garage.veicoli.add(new Auto("Audi"));
 		garage.veicoli.add(new Camion("//"));
