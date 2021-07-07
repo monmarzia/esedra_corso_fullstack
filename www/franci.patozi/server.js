026e656239fcae58709c2bd05aa94d0cc4fd91d9
Franci110 <francip079@gmail.com>
2021-07-07 18:21:48 +0200

    cartella collection

---
+++ www/franci.patozi/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/oggetti', (req, res) => {
+  res.sendFile(__dirname + '/oggetti.html');
+});
+
+app.get('/array', (req, res) => {
+  console.log("chiamata alla rotta array");
+  res.sendFile(__dirname + '/array.html');
+});
+
+app.get('/mm_array', (req, res) => {
+  res.sendFile(__dirname + '/monica.marziani' +'/array.html');
+});
+
+app.listen(port, () => console.log(`in ascolto alla porta ${port}`));
