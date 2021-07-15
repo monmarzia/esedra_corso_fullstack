5c0bae983fe23edb54a08a098ec3f9648041e070
monmarzia <monica.marziani@aruba.it>
2021-07-15 12:47:40 +0200

    Implementazione e test di User get() e User getSequence()

--- src/it/esedra/corso/shoppinglist/model/User.java
+++ src/it/esedra/corso/shoppinglist/model/User.java
@@ -3,42 +3,41 @@ package it.esedra.corso.shoppinglist.model;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.util.List;
 
 import it.esedra.corso.shoppinglist.helper.GetFileResource;
 
-/**
- * 
- */
 /**
  * @author monica
  *
  */
 public class User implements Persist {
-	private static BigInteger userId = new BigInteger("1");
+	private static BigInteger userId = new BigInteger("0");
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String mobilePhone;
-	private boolean isActive;
-	private boolean privacyConsent;
-	private boolean newsletter;
+	private boolean isActive = false;
+	private boolean privacyConsent = false;
+	private boolean newsletter = false;
 
 	public User() {
 		
 	}
 	
-	public User(String firstName, String lastName, String email, String mobilePhone) {
+	public User(String firstName, String lastName, String email, String mobilePhone, boolean isActive, boolean privacyConsent, boolean newsletter) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.mobilePhone = mobilePhone;
-		isActive = false;
-		privacyConsent = false;
-		newsletter = false;
+		this.isActive = isActive;
+		this.privacyConsent = privacyConsent;
+		this.newsletter = newsletter;
 	}
 	
 	public User build() {
-		return new User(firstName, lastName, email, mobilePhone);
+		return new User(firstName, lastName, email, mobilePhone, isActive, privacyConsent, newsletter);
 	}
 	
 	/**
@@ -100,8 +99,9 @@ public class User implements Persist {
 		return privacyConsent;
 	}
 	
-	public void setPrivacyConsent(boolean privacyConsent) {
+	public User setPrivacyConsent(boolean privacyConsent) {
 		this.privacyConsent = privacyConsent;
+		return this;
 	}
 
 	/**
@@ -129,8 +129,9 @@ public class User implements Persist {
 	/**
 	 * @param isActive the isActive to set
 	 */
-	public void setActive(boolean isActive) {
+	public User setActive(boolean isActive) {
 		this.isActive = isActive;
+		return this;
 	}
 
 	/**
@@ -143,14 +144,75 @@ public class User implements Persist {
 	/**
 	 * @param newsletter the newsletter to set
 	 */
-	public void setNewsletter(boolean newsletter) {
+	public User setNewsletter(boolean newsletter) {
 		this.newsletter = newsletter;
+		return this;
 	}
+	/**
+	 * 
+	 * @return userID incrementato di 1
+	 * 
+	 * Testato l'incremento: il secondo user ha userId = 2;
+	 */
 	
 	public BigInteger getSequence() {
-		return	userId.add(new BigInteger("1"));
+		return userId =	userId.add(new BigInteger("1"));
 	}
 
+	/**
+	 * Restituisce un nuovo oggetto User
+	 * 
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	
+	/**
+	 * 
+	 * Modifica: aggiunto blocco try/catch per gestire l'errore ed evitare l'uso del blocco
+	 * nella classe TestUser.
+	 * 
+	 * Modificato return (era "new User()" vuoto)
+	 * 
+	 * Modificato Il BufferedReader con il metodo statico Files.readAlLines(path) che restituisce
+	 * una List di String ciascuna con una linea. Utilizzato un ciclo for migiorato per la ricerca
+	 * dello User nel file user.csv.
+	 * 
+	 */
+	public User get() throws IOException {
+		try {
+			List<String> lines = Files.readAllLines(GetFileResource.get("user.csv", "shoppinglist").toPath());
+			User user = null;
+			for(String line:lines) {				
+				String[] fields = line.split(",");
+				BigInteger tmpUserId = new BigInteger(fields[0]);
+				if (tmpUserId.equals(this.getUserId())) {
+					user = new User();
+					user.setActive(Boolean.parseBoolean(fields[5]));
+					user.setEmail(fields[3]);
+					user.setFirstName(fields[1]);
+					user.setLastName(fields[2]);
+					user.setMobilePhone(fields[4]);
+					user.setNewsletter(Boolean.parseBoolean(fields[7]));
+					user.setPrivacyConsent(Boolean.parseBoolean(fields[6]));
+					user.setUserId(tmpUserId);
+				}
+			}
+			return user;
+		} catch (Exception e) {
+			throw new IOException();
+		}
+				
+	}
+	
+	/**
+	 * Salva un oggetto user
+	 * 
+	 * Problema: Il file viene sovrascritto ad ogni chiamata del metodo.
+	 * Il problema riguarda anche ShoppingList in quanto non possono essere salvate
+	 * sul file due diverse liste, ma solo una per volta
+	 * 
+	 */
 	public void store() throws IOException {
 		try {
 			PrintWriter writer = new PrintWriter(GetFileResource.get("user.csv", "shoppinglist"));
