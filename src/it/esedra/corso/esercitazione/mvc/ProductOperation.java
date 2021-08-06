5bc23ee90f8a41eb8e7cfdfaa6aec8e9316b57e5
ThomasMarchese <Thomasmarkese@hotmail.it>
2021-08-06 18:37:41 +0200

    aggiornamenti a productOperation

--- src/it/esedra/corso/esercitazione/mvc/ProductOperation.java
+++ src/it/esedra/corso/esercitazione/mvc/ProductOperation.java
@@ -47,24 +47,16 @@ public class ProductOperation implements Operation<Product>{
 	@Override
 	public void validate(String[] params) throws ValidateException{	
 		
-		for(int i = 1 ; i < params.length ; i++) {
+		//type,action,id,name,price
 		/*
-		 * verifica che non ci siano celle null o vuote
+		 * verifica che la cella 1 non sia null o vuota
 		 * parte dalla seconda cella dato che la 1a è stata precedentemente verificata
 		 */
-			if(params[i] == null || params[i].equals(""))
-		
-				throw new ValidateException("params[" + i + "] cannot be null or empty ");
-		}
-		/* 
-		 * verifica che nella cella che dovrebbe contenere il nome non siano presenti numeri
-		 */
-		
-		for(int i = 0 ; i <= 9  ; i++) {
-			System.out.println(i);
-			if(params[1].contains(String.valueOf(i)))
-				throw new ValidateException("params[" + 1 + "] cannot contain numbers ");
-		}
+			if(params[1] == null || params[1].equals("")) {
+				throw new ValidateException("params[" + 1 + "] cannot be null or empty ");
+			}
+				
+
 		
 		/* 
 		 * prova a sfruttare il costruttore di BigInteger(String) con la cella che dovrebbe contenere l'id
@@ -77,16 +69,16 @@ public class ProductOperation implements Operation<Product>{
 		}
 		
 		/* 
-		 * prova a sfruttare il costruttore di BigDecimal(String) con la cella che dovrebbe contenere il prezzo
+		 * prova a sfruttare il costruttore di BigDecimal(String) con la cella che dovrebbe contenere il prezzo 
 		 * se genera una eccezione allora significa che non riesce a parsare , sicuramente non è un prezzo giusto
 		 */	
 		try {
-			new BigDecimal(params[3]);   
+			new BigDecimal(params[4]);   
 		} catch (Exception e) {
-			throw new ValidateException("params[" + 3 + "] is not a BigDecimal ");
+			throw new ValidateException("params[" + 4 + "] is not a BigDecimal ");
 		}
 		
-	}
+	}  
 	
 	@Override
 	public void process(String[] params) throws OperationException {
