362c9d65ceac096f8c4912764de329dc8eb12a73
monmarzia <monica.marziani@aruba.it>
2021-09-19 15:50:07 +0200

    Delete all ShoppingList resources

--- src/it/esedra/corso/shoppinglist/model/Unit.java
+++
@@ -1,5 +0,0 @@
-package it.esedra.corso.shoppinglist.model;
-
-public enum Unit {
-	NUMBER, KG, HG, G, L, CM, M;
-}
--- src/it/esedra/corso/shoppinglist/model/Validate.java
+++
@@ -1,8 +0,0 @@
-package it.esedra.corso.shoppinglist.model;
-
-import it.esedra.corso.esercitazione.mvc.ValidateException;
-
-public interface Validate {
-	public void validate(String params) throws ValidateException;
-
-}
